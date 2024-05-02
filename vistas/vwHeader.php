4f1d211d8f8eae6476692515ab35404b8f6d83ea
David Soriano <davidscicua314@gmail.com>
2024-05-01 19:07:48 -0500

    Prime commit del proyecto

---
+++ vistas/vwHeader.php
@@ -0,0 +1,27 @@
+<header>
+    <div class="row">
+        <div class="col-5">
+            <div class="col-1">
+                <i class="bi bi-three-dots-vertical" title="Menú"></i>
+            </div>
+            <ul>
+                <li><a href="#">AYUDA</a></li>
+                <li><a href="#">PERFIL</a></li>
+            </ul>
+        </div>
+        <div class="col-2">
+            <img src="img/logo.png" alt="Logo">
+        </div>
+        <div class="col-5">
+            <div class="col bx-menurg">
+                <button><a href="#">Sign In</a></button>
+                <button><a href="#">Sign Up</a></button>
+                <div class="col-2">
+                    <a href="#"><i class="bi bi-search-heart" title="Buscar"></i></a>
+                    <a href="#"><i class="bi bi-box-arrow-left" id="btn-exit" title="Salir"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+</header>
---
+++ vistas/wFooter.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="row">
+        <div class="col-2">
+            <h6>&copy; By Grupo 1 - ADSO</h6>
+            <div class="col">
+                <a href="#"><i class="bi bi-facebook"></i></a>
+                <a href="#"><i class="bi bi-instagram"></i></a>
+                <a href="#"><i class="bi bi-github"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
